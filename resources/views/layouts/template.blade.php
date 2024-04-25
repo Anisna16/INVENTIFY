a68bb1ca8eed74ae3c1b4eab2fe5b00189c5b0ce
Anisna Hilwa Nadhifah <nadhifahanisna@gmail.com>
2024-04-25 14:48:11 +0700

    updated logo

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -31,7 +31,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
-      <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ asset('https://img2.pngdownload.id/20180330/oue/kisspng-letter-alphabet-i-z-clip-art-information-5abe4a33246c50.9034660915224202751492.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">INVENTIFY</span>
     </a>
 
